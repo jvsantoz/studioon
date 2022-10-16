fc3d2ae11975d30873781994f8d6ffd28273971b
jvsantoz <joao.vitor.santossilva.cba@gmail.com>
2022-10-16 17:33:58 -0400

    feat:(html, css, javascrip) visibility on mobile

--- SCR/script.js
+++ SCR/script.js
@@ -1,4 +1,13 @@
 $(document).ready(() => {
+
+  //MENU 
+$('.navbar__menu-btn').on('click', function () {
+  $('.navbar__links').toggleClass('active');
+  $(this).find('i').toggleClass('fa-bars');
+  $(this).find('i').toggleClass('fa-times');
+})
+
+  // CARROSSEL 
   const slickOptions = {
     autoplay: true,
     dots: false,
